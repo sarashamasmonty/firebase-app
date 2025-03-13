bd43221c75ad7579c08aa4da91fc69ff4f21e00e
Sara Shamas <sara.shamas@mymonty.com>
2025-03-13 11:06:52 +0200

    Firebase Initialisation

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,32 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getAnalytics, provideAnalytics, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
+import { initializeAppCheck, ReCaptchaEnterpriseProvider, provideAppCheck } from '@angular/fire/app-check';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getFunctions, provideFunctions } from '@angular/fire/functions';
+import { getMessaging, provideMessaging } from '@angular/fire/messaging';
+import { getPerformance, providePerformance } from '@angular/fire/performance';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import { getRemoteConfig, provideRemoteConfig } from '@angular/fire/remote-config';
+import { getVertexAI, provideVertexAI } from '@angular/fire/vertexai-preview';
+import { firebaseConfig } from '../environments/firebase-config';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
+  provideRouter(routes),
+  provideFirebaseApp(() => initializeApp(firebaseConfig)),
+  provideAuth(() => getAuth()), provideAnalytics(() => getAnalytics()),
+  ScreenTrackingService,
+  provideFirestore(() => getFirestore()),
+  provideDatabase(() => getDatabase()),
+  provideFunctions(() => getFunctions()),
+  provideMessaging(() => getMessaging()),
+  providePerformance(() => getPerformance()),
+  provideStorage(() => getStorage()),
+  provideRemoteConfig(() => getRemoteConfig()),
+  provideVertexAI(() => getVertexAI())]
 };
---
+++ src/environments/firebase-config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = 
+{ "projectId": "fir-app-89b1c", 
+    "appId": "1:620337207590:web:68b94bc660ee39482149e7", 
+    "storageBucket": "fir-app-89b1c.firebasestorage.app", 
+    "apiKey": "AIzaSyD5cJxWazH__1hgQY7f7Ysj6FOcVTdZVaY", 
+    "authDomain": "fir-app-89b1c.firebaseapp.com", 
+    "messagingSenderId": "620337207590", 
+    "measurementId": "G-7X90G2LYNR" }
